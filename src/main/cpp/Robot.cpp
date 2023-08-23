80b65ef2fa444b23f638e4ce3cd843f80a118fbc
Stella <crusher5000@outlook.com>
2023-08-23 15:58:02 +0800

    update Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -66,17 +66,17 @@ void Robot::TeleopPeriodic()
 	// Drive newMec(0.02, 0.8);
 	// // call MecDrive for mecanum drive control
 	
-	frc::SlewRateLimiter<units::scalar> filterY{0.5 / 1_s};	
-	// Smooth the joystick X for every unit of time
-	frc::SlewRateLimiter<units::scalar> filterX{0.5 / 1_s};
+	// frc::SlewRateLimiter<units::scalar> filterY{0.5 / 1_s};	
+	// // Smooth the joystick X for every unit of time
+	// frc::SlewRateLimiter<units::scalar> filterX{0.5 / 1_s};
 
 	//Applies the slew rate limiter
-	double joyY = filterY.Calculate(joystick.GetY());
-	double joyX = filterX.Calculate(-joystick.GetX());
+	// double joyY = filterY.Calculate(joystick.GetY());
+	// double joyX = filterX.Calculate(-joystick.GetX());
 
 	// squares joystick intensity for finer control. This is not done for mecanum drive but is automatically done for drive train
-	double joyYPower = joyY * fabs(joyY);
-	double joyXPower = joyX * fabs(joyX);
+	double joyYPower = joystick.GetY() * fabs(joystick.GetY());
+	double joyXPower = -joystick.GetX() * fabs(joystick.GetX());
 	
 	// y speed, x speed, rotation, feild orientation compensation angle
 	mec_drive.DriveCartesian(joyYPower, joyXPower, -joystick.GetZ());
