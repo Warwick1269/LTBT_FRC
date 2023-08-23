d94ecdc629fedc1dce2274d1d7360db663d5351c
Stella <crusher5000@outlook.com>
2023-08-23 15:22:25 +0800

    update Arm.cpp

--- src/main/cpp/Arm.cpp
+++ src/main/cpp/Arm.cpp
@@ -62,6 +62,9 @@ void Arm::ArmBendTwo(double maxSpeed, double smooth)
  */
 void Arm::Intake(double speed)
 {
+	int _lBumper = controller.GetRawButton(5);
+	int _rBumper = controller.GetRawButton(6);
+
     if (_rBumper)
     {
         intake1.Set(speed);
